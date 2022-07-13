052eb8a93621ecf08992c970ea59d886137bf4a5
purebeast46 <nhdnahid2017@gmail.com>
2022-07-13 21:29:40 +0600

    v

---
+++ hashCode.js
@@ -0,0 +1,5 @@
+function hashCode(str) {
+    return str.split('').reduce((prevHash, currVal) =>
+      (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
+  }
+  console.log("hashCode(\"Hello!\"): ", hashCode('Hello!'));
